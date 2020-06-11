6326c3ed7f7e1661d3dbc7fd02830eef14929a90
Alex Beciana <alex.beciana@gmail.com>
2020-06-11 21:53:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     inner_index = 0 
     while inner_index < src[outer_index].length do
       if src[outer_index][inner_index].class == String
-        puts src[outer_index][inner_index].join
+        extract << src[outer_index][inner_index].join(" ")
       end
       
       inner_index += 1 
