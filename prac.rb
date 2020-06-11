a05a4768aa92a4406557d7cf31e322698a0b70df
Alex Beciana <alex.beciana@gmail.com>
2020-06-11 21:49:20 +0000

    Automatically backed up by Learn

---
+++ prac.rb
@@ -0,0 +1,21 @@
+
+
+
+def join_nested_strings(src)
+  outer_index = 0 
+  while outer_index < src.length do
+    inner_index = 0 
+    while inner_index < src[outer_index].length do
+      if src[outer_index][inner_index] == ""
+        puts src[outer_index][inner_index].join
+      end
+      
+      inner_index += 1 
+    end
+    
+    outer_index += 1 
+  end
+  # src will be an Array of Arrays of Strings and Integers
+  # Combine all Strings present in the AoA into a single value and return it
+  
+end
