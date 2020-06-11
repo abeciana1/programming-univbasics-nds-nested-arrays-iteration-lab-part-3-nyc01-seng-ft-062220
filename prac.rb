229602e5e26b9ff3187f7168a093b844e33faa75
Alex Beciana <alex.beciana@gmail.com>
2020-06-11 21:50:18 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
   while outer_index < src.length do
     inner_index = 0 
     while inner_index < src[outer_index].length do
-      if src[outer_index][inner_index] == ""
+      if src[outer_index][inner_index] == String
         puts src[outer_index][inner_index].join
       end
       
